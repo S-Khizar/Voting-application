a07448b213f7078bdf8ec36f90bef82200c06f92
Khizar <solkarkhizar27@gmail.com>
2024-03-07 13:53:14 +0530

    update the route

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -8,7 +8,7 @@ const { message } = require('prompt');
 const checkAdminRole = async (userID)=>{ 
     try{
         const user = await User.findById(userID);
-        return user && user.role === 'admin';
+        return user && user.role == 'admin';
     }catch(err){
         return false;
     }
@@ -17,7 +17,7 @@ const checkAdminRole = async (userID)=>{
 //POST route to add a candidate
 router.post('/',jwtAuthMiddleware,async(req,res)=>{
     try{
-        if (!(await checkAdminRole(req.user.userData.id))) {
+        if (!(await checkAdminRole(req.user.id))) {
             return res.status(403).json({ message: 'User does not have admin role' });
         }
         
@@ -43,8 +43,9 @@ module.exports = router;
 
 router.put('/:candidateID',jwtAuthMiddleware,async(req,res)=>{
     try{
-        if(!checkAdminRole(req.user.id)){
-            return res.status(403).json({message : 'user does not have admin role'});
+        
+        if (!(await checkAdminRole(req.user.id))) {
+            return res.status(403).json({ message: 'User does not have admin role' });
         }
         const candidateID = req.params.candidateID;//Extract the id from URL parameter
         const updateCandidateData = req.body; //update data for emp
@@ -66,8 +67,9 @@ router.put('/:candidateID',jwtAuthMiddleware,async(req,res)=>{
 
 router.delete('/:candidateID',jwtAuthMiddleware,async(req,res)=>{
     try{
-        if(!checkAdminRole(req.user.id)){
-            return res.status(403).json({message : 'user does not have admin role'});
+        
+        if (!(await checkAdminRole(req.user.id))) {
+            return res.status(403).json({ message: 'User does not have admin role' });
         }
         const candidateID = req.params.candidateID;//Extract the id from URL parameter
         
@@ -83,6 +85,70 @@ router.delete('/:candidateID',jwtAuthMiddleware,async(req,res)=>{
     }catch(err){
         console.log(err);
         res.status(500).json({error : 'Internal Server Error'});
+    }
+})
+
+//let's start voting
+router.post('/vote/:candidateID',jwtAuthMiddleware,async(req,res)=>{
+    //no admin can vote
+    //user can only vote one
+    candidateID = req.params.candidateID;
+    console.log(req.params.candidateID);
+    userID = req.user.id;
+    try{
+        //Find the Candidate document with the specified candidateID
+        const candidate = await Candidate.findById(candidateID);
+        if(!candidate){
+            return res.status(404).json({message : 'Candidate not found'});
+        }
+
+        const user = await User.findById(userID);
+        if(!user){
+            return res.status(404).json({message : 'User not found'});
+        }
+        if(user.isVoted){
+             res.status(400).json({message : 'You have already voted'});
+        }
+        if(user.role == 'admin'){
+            res.status(403).json({message : 'admin is not allowed'});
+        }
+        //update the candidate document to record the vote
+        candidate.votes.push({user : userID});
+        candidate.voteCount++;
+        await candidate.save();
+
+        //update the user document
+        user.isVoted = true;
+        await user.save();
+        
+        res.status(200).json({message : 'Vote recorded succesfully'});
+
+
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({err : 'Internal server error'});
+    }
+});
+
+//vote  count
+router.get('/vote/count',async (req,res)=>{
+    try{
+        //Find all candidate and sort them by vote count in descending order
+    const candidate = await Candidate.find().sort({voteCount : 'desc'});
+
+    //Map the candidate to only return their names and vote count
+    const voteRecord = candidate.map((data)=>{
+        return{
+            name : data.name,
+            party: data.party,
+            count : data.voteCount
+        }
+    });
+    return res.status(200).json(voteRecord);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({err : 'Internal server error'});
     }
 })
 
